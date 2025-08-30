d1c5736e28fa2bbb80b1fd0106cb4934845e29a6
Manas Tiwari <manastiwari2021@gmail.com>
2025-08-30 12:51:12 +0000

    you direclty put the firebase Credentials in the code no need to put the

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "habitual-harmony-am5y3",
-  appId: "1:373849160259:web:fc30d88d23ce11386cf2f6",
-  storageBucket: "habitual-harmony-am5y3.appspot.com",
   apiKey: "AIzaSyDwbczEeZXexQfBRVIHYVEPdvGruetABLE",
   authDomain: "habitual-harmony-am5y3.firebaseapp.com",
+  projectId: "habitual-harmony-am5y3",
+  storageBucket: "habitual-harmony-am5y3.appspot.com",
   messagingSenderId: "373849160259",
+  appId: "1:373849160259:web:fc30d88d23ce11386cf2f6"
 };
 
 // Initialize Firebase
